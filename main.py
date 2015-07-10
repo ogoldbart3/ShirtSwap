4877bab7d5ee648408313030fcff54fa01562bea
Oliver Goldbart <oliver.goldbart@gmail.com>
2015-07-10 15:02:46 -0400

    Added test file and image

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import os
 import numpy as np
-import cv2
+import cv2 as cv
 import scipy.signal
 
 print "hello"
+imgFile = cv.imread('1.jpg')
+
+cv.imshow('image', imgFile)
+cv.waitKey(0)
+cv.destroyAllWindows()
